263d3e09d7d0aaacfa5a2e1bc55bcc67351d084e
Sacred <mafiadon00@yandex.ru>
2017-10-28 15:05:01 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Документы</title>
+</head>
+<body><!-- Комментарий -->
+<header>
+
+</header>
+
+
+
+<section>
+
+
+
+</section>
+
+
+
+
+
+
+
+
+
+<footer>
+
+</footer>
+
+
+</body>
+</html>
+
+<?php
+?>
