c38a71d5683d1f4f3f747cb26ed269a036df24aa
Sacred <mafiadon00@yandex.ru>
2017-10-28 21:33:14 +0300

    table+footer

--- index.php
+++ index.php
@@ -61,18 +61,152 @@
                     <option value="toserver">Опция</option>
                 </select></div>
         </section>
-        <div class="button"><input type="button" value="Сброс"><input type="button" value="Сортировать"></div>
+        <div class="button-header"><input type="reset"><input type="button" value="Сортировать"></div>
     </header>
 
 
     <section class="table">
-table
+
+        <table width="100%" border="1" cellspacing="0" cellpadding="4">
+
+            <tr>
+                <th>&nbsp;</th>
+                <th>Чебурашка</th>
+                <th>Крокодил Гена</th>
+                <th>Шапокляк</th>
+                <th>Чебурашка</th>
+                <th>Крокодил Гена</th>
+                <th>Шапокляк</th>
+                <th>Чебурашка</th>
+                <th>Крокодил Гена</th>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb1"></td>
+                <td>5</td>
+                <td>2</td>
+                <td>1</td>
+                <td>Съел, кг</td>
+                <td>5</td>
+                <td>2</td>
+                <td>1</td>
+                <td>Съел, кг</td>
+
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb2"></td>
+                <td>6</td>
+                <td>8</td>
+                <td>2</td>
+                <td>Выпил, л</td>
+                <td>6</td>
+                <td>8</td>
+                <td>2</td>
+                <td>Выпил, л</td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb3"></td>
+                <td>5</td>
+                <td>5</td>
+                <td>1</td>
+                <td>Смог, раз</td>
+                <td>5</td>
+                <td>5</td>
+                <td>1</td>
+                <td>Смог, раз</td>
+
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb4"></td>
+                <td>5</td>
+                <td>2</td>
+                <td>1</td>
+                <td>Съел, кг</td>
+                <td>5</td>
+                <td>2</td>
+                <td>1</td>
+                <td>Съел, кг</td>
+
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb5"></td>
+                <td>6</td>
+                <td>8</td>
+                <td>2</td>
+                <td>Выпил, л</td>
+                <td>6</td>
+                <td>8</td>
+                <td>2</td>
+                <td>Выпил, л</td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb6"></td>
+                <td>5</td>
+                <td>5</td>
+                <td>1</td>
+                <td>Смог, раз</td>
+                <td>5</td>
+                <td>5</td>
+                <td>1</td>
+                <td>Смог, раз</td>
+
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb7"></td>
+                <td>5</td>
+                <td>2</td>
+                <td>1</td>
+                <td>Съел, кг</td>
+                <td>5</td>
+                <td>2</td>
+                <td>1</td>
+                <td>Съел, кг</td>
+
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb8"></td>
+                <td>6</td>
+                <td>8</td>
+                <td>2</td>
+                <td>Выпил, л</td>
+                <td>6</td>
+                <td>8</td>
+                <td>2</td>
+                <td>Выпил, л</td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="cb9"></td>
+                <td>5</td>
+                <td>5</td>
+                <td>1</td>
+                <td>Смог, раз</td>
+                <td>5</td>
+                <td>5</td>
+                <td>1</td>
+                <td>Смог, раз</td>
+
+            </tr>
+        </table>
+
 
     </section>
 
 
     <footer>
-footer
+        <div class="button-footer">
+            <div class="plus-minus">
+                <input type="button" value="+">
+                <input type="button" value="-">
+            </div>
+            <div class="other-footer">
+                <input type="button" value="Карточка документа" disabled>
+                <input type="button" value="Просмотр" disabled>
+                <input type="button" value="Открыть документ" disabled>
+                <input type="button" value="Экспорт в Exel" disabled>
+                <input type="button" value="Дублировать запись" disabled>
+            </div>
+            <div style="width: 40px">
+            </div>
+        </div>
     </footer>
 
 </div>
