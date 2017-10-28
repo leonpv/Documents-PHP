11652e1a09c491779a241c86ddc80d63efa616de
Sacred <mafiadon00@yandex.ru>
2017-10-28 20:17:20 +0300

    header

--- index.php
+++ index.php
@@ -1,36 +1,81 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta charset=utf-8">
     <title>Документы</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body><!-- Комментарий -->
-<header>
+<body>
+<div class="container">
+    <header>
+        <section class="headerQWE">
+            <div>
+                <label for="level">Уровень</label><br>
+                <select id="level">
+                    <option value="NULL" selected></option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                </select></div>
+            <div><label for="level">Утверждающая орг-ция</label><br>
+                <select id="level">
+                    <option value="NULL" selected></option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                </select></div>
+            <div><label for="level">Тип документа</label><br>
+                <select id="level">
+                    <option value="NULL" selected></option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                </select></div>
+            <div><label for="level">Тип объекта</label><br>
+                <select id="level">
+                    <option value="NULL" selected></option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                </select></div>
+            <div style="align-self: flex-end"><label for="level">Год утвержд.</label><br>
+                <select id="level">
+                    <option value="NULL" selected></option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                </select></div>
+            <div><label for="level">Номер документа</label><br>
+                <input type="search"></div>
+            <div><label for="level">Название документа</label><br>
+                <input type="search"></div>
+            <div><label for="level">Аннотация и обл. прим.</label><br>
+                <input type="search"></div>
+            <div><label for="level">№ и дата приказа о вводе</label><br>
+                <input type="search"></div>
+            <div style="align-self: flex-start"><label for="level">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp—</label><br>
+                <select id="level">
+                    <option value="NULL" selected></option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                    <option value="toserver">Опция</option>
+                </select></div>
+        </section>
+        <div class="button"><input type="button" value="Сброс"><input type="button" value="Сортировать"></div>
+    </header>
 
-</header>
+
+    <section class="table">
+table
+
+    </section>
 
 
+    <footer>
+footer
+    </footer>
 
-
-<section>
-
-
-
-</section>
-
-
-
-
-
-
-
-
-
-<footer>
-
-</footer>
-
-
+</div>
 </body>
 </html>
 
